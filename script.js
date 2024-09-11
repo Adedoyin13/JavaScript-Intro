7fb6cb746e07569195377b2424a67c2e9a33e71e
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2024-09-11 13:00:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,134 @@
+// Data Types
+
+// Primitive Data Type
+
+// Number Data Type
+
+const b = 2;
+
+const c = 6;
+
+console.log(b, c);
+
+// Boolean Data Type
+
+let isStudent = true;
+
+let hasGraduated = false;
+
+console.log(typeof hasGraduated);
+
+// Undefined Data Type
+
+let soliu;
+
+console.log(typeof soliu);
+
+// Null Data Type
+
+let myName = null;
+
+console.log(typeof myName)
+
+// Non-Primitive Data Type
+
+// Object Data Type
+
+let person = {
+    name:"Doyin",
+    age: 2,
+    isCohort: true,
+};
+
+console.log(person);
+
+// Array Data Types
+
+let colors = ["red", "green", "black"];
+
+console.log(colors.length);
+console.log(colors[2]);
+console.log(typeof colors);
+
+let person1 = [
+    {
+        name: 'Ali',
+        color: 'red'
+    },
+    {   name: 'Anate',
+        color: 'burgundy'
+    }
+]
+
+console.log(person1);
+
+//Operators
+
+let a = 10;
+let d = 5;
+
+// Addition
+
+let sum = a + d ;
+
+console.log(sum);
+
+
+// Subtraction
+
+let subtraction = a - d ;
+
+console.log(subtraction);
+
+
+// Multiplication
+
+let multiply = a * d ;
+
+console.log(typeof multiply);
+
+
+// Division
+
+let divide = a / d ;
+
+console.log(divide);
+
+
+// Modulus (reminder)
+
+let remainder = a % d ;
+
+console.log(remainder);
+
+
+// Exponentiation (reminder)
+
+let power = a ** 2 ;
+
+console.log(power);
+
+// Comparison Operators are used to compare two values and return a boolean attribute
+
+// Comparison Operators
+
+let x = 10;
+let y = 20;
+
+console.log(x == y);
+console.log(x != y);
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+
+// Equality Operators
+
+let myVariable = '1';
+let myNumber = 1;
+
+console.log(myVariable == myNumber);
+console.log(myVariable === myNumber);
+
+
+
